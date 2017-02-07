8e042354f7992b9188598336f2cd5b3110ba2b20
mtmcduffie <mcduffie.michael@gmail.com>
2017-02-07 13:15:16 -0500

    PPM-57 - Using custom class for JWT management.

--- SciReg/settings.py
+++ SciReg/settings.py
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'SciReg.auth0authenticate.Auth0JSONWebTokenAuthentication',
     ),
 }
 
