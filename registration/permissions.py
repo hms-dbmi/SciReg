602175099ec7240df9d78ffd112301b79d4c6093
mtmcduffie <mcduffie.michael@gmail.com>
2017-05-10 09:35:15 -0400

    PPM-82 - Creating user if they don't exist in registration system when using API.

--- registration/permissions.py
+++ registration/permissions.py
@@ -12,8 +12,18 @@ class IsAssociatedUser(permissions.BasePermission):
 
 def jwt_get_username_from_payload(payload):
     """
-    Custom method that forces username to be e-mail address.
-    :param payload:
-    :return:
+    Method to create user in SciReg Application if they don't exist.
     """
+
+    print("[SCIREG][DEBUG][jwt_get_username_from_payload] - Attempting to Authenticate User - " + payload.get('email'))
+
+    try:
+        User.objects.get(username=payload.get('email'))
+    except User.DoesNotExist:
+
+        print("[SCIREG][DEBUG][jwt_get_username_from_payload] - User not found, creating.")
+
+        user = User(username=payload.get('email'), email=payload.get('email'))
+        user.save()
+
     return payload.get('email')
